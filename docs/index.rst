1e4b28ddd675318369a704c83201e8ce11bee3d5
Philip Brechler <pbrechler@mac.com>
2021-03-18 10:15:49 +0100

    Added first docs

--- docs/index.rst
+++ docs/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to joto_api's documentation!
+joto_api
 ====================================
 
 .. toctree::
@@ -18,3 +18,4 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
--- joto_api/__init__.py
+++ joto_api/__init__.py
@@ -5,6 +5,34 @@ ROOT_DOMAIN = "https://api.joto.io"
 OAUTH_ENDPOINT = "https://those-apps.auth.eu-west-1.amazoncognito.com"
 
 class JotoAPI:
+    """
+    The class representing the tools to interact with the Joto API
+
+    ...
+
+    Attributes
+    ----------
+    client_id : str
+        The client id used, usually given in the construction method
+    client_secret : str
+        The client secret used to communicate with the server, usually given in the construction method
+    token : str
+        The token used to communicate with the API, see get_token() method
+
+    Methods
+    -------
+    get_token()
+        A method to get an access token with the client id and secret. Handled automatically when using other methods
+    create_jot(jot)
+        Send a JotObject to the server in order to start processing. The methods returns as soon as the server has processed the code and the Jot is ready to be sent
+    fetch_jot(jot_id)
+        Used by wait_for_jot_ready(jot) to fetch the jot after creation to check for ready state
+    send_jot_id_to_playlist(jot_id,playlist)
+        Takes the jot_id after creating a jot and sends it to a given playlist id.
+    wait_for_jot_ready(retries=5)
+        Internal method to fetch a jot and check if GCODE is processed, will retry to get data every 3 seconds
+    """
+
     client_id = None
     secret = None
     token = None
@@ -75,6 +103,34 @@ class JotoAPI:
             return None
 
 class JotObject:
+    """
+    The class representing a Jot that is sent to the joto servers
+
+    ...
+
+    Attributes
+    ----------
+    title : str
+        The title of the jot, shows on the user interface after drawing
+    description : str
+        The description of the Jot, also shows in the user interface to users
+    svg : str
+        The svg content of the draw, needs to be valid SVG in order to be drawn successfully
+    categories : list
+        A list of strings that represent the categories of the Jot (optional)
+    tags : list
+        A list of strings that represent tags (optional)
+    partMeta: dict
+        A dict with meta data (optional)
+    meta: dict
+        A dict with meta data (optional)
+
+    Methods
+    -------
+    to_dict()
+        Returns a dict represantation of the jot to be used when sent to server
+    """
+
     def __init__(self,title,description,svg,categories=[],tags=[],partMeta={},meta={}):
         self.title = title
         self.description = description
